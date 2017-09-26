6af089fbbe7b0c812fd06ba1c3b3bb30d2cfe72c
rodrigogrocha@hotmail.com <Cruzeiro.2015>
2017-09-26 08:37:32 -0500

    First Commit

---
+++ src/HelloPrinter.java
@@ -0,0 +1,8 @@
+public class HelloPrinter
+{
+   public static void main(String[] args) 
+   {
+       System.out.print("30 / 0 = ");
+       System.out.println(30 / 0);
+   }
+}
---
+++ src/NumberDemoRocha.java
@@ -0,0 +1,45 @@
+
+public class NumberDemoRocha {
+
+	public static void main(String[] args) {
+
+		int numberDaysNotLeapYear = 365;
+		double balance = 100.00;
+		float floatVariable = 234.5f;
+		boolean courseIsGreat = true;
+		short currentYear = 2017;
+		long distanceToMoon = 238900L;
+		byte numberOfWeeksYear = 52;
+		char firstLetter = 'A';
+		
+		System.out.println("Number of days in a non leap year: " + numberDaysNotLeapYear);
+		System.out.println("My account balance is: " + balance);
+		System.out.println("This is a float variable: " + floatVariable);
+		System.out.println("Is this course great?: " + courseIsGreat);
+		System.out.println("Our current year is: " + currentYear);
+		System.out.println("Distance to the moon is: " + distanceToMoon);
+		System.out.println("Number of weeks in a year: " + numberOfWeeksYear);
+		System.out.println("First letter in the alphabet is: " + firstLetter);
+		
+		final int NUMBER_OF_EMPLOYEES = 1000;
+		final double AMOUNT_OWED = 1500.00;
+		final float AVERAGE_HEIGHT = 5.9f;
+		final boolean COURSE_COMPLETION = false;
+		final short COURSE_WEEKS = 30;
+		final long USA_POPULATION = 323100000L;
+		final byte AGE_TO_RETIRE = 65;
+		final char LAST_LETTER = 'Z';
+		
+		System.out.println();
+		System.out.println("Company has: " + NUMBER_OF_EMPLOYEES + " employees");
+		System.out.println("He owes me: " + AMOUNT_OWED);
+		System.out.println("Person's average height is: " + AVERAGE_HEIGHT + " feets.");
+		System.out.println("Have we completed the course yet? " + COURSE_COMPLETION);
+		System.out.println("The course is " + COURSE_WEEKS + " weeks long.");
+		System.out.println("USA's population is: " + USA_POPULATION);
+		System.out.println("Ideal age to retire is: " + AGE_TO_RETIRE);
+		System.out.println("Last letter is: " + LAST_LETTER);
+
+	}
+
+}
